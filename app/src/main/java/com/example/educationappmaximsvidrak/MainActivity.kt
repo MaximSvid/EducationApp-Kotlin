5bc506dee087410f2dd606483566b9098d8cbfd5
Maxim Svidrak <maximsvid@gmail.com>
2024-08-13 13:29:07 +0200

    13.08.24 bottom_nav_menu.xml version 2.2

--- app/src/main/java/com/example/educationappmaximsvidrak/MainActivity.kt
+++ app/src/main/java/com/example/educationappmaximsvidrak/MainActivity.kt
@@ -1,22 +1,13 @@
 package com.example.educationappmaximsvidrak
 
-import android.annotation.SuppressLint
 import android.os.Bundle
-import android.widget.EditText
-import android.widget.ImageButton
-import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
-import androidx.recyclerview.widget.RecyclerView
 import com.example.educationappmaximsvidrak.databinding.ActivityMainBinding
 //import com.example.educationappmaximsvidrak.adapter.ChatGPTAdapter
-import com.example.educationappmaximsvidrak.model.Message
-import okhttp3.OkHttpClient
 
 class MainActivity : AppCompatActivity() {
 
@@ -29,12 +20,24 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView2) as NavHostFragment
-        val navController = navHostFragment.navController
-        binding.bottomNavigation.setupWithNavController(navController)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        binding.bottomNavigation.setupWithNavController(navHostFragment.navController)
+
+        navHostFragment.navController.addOnDestinationChangedListener {_, destination, _ ->
+            when(destination.id) {
+                R.id.chatGPTFragment,
+                R.id.startFragment,
+                R.id.addQuestionFragment -> {binding.bottomNavigation.visibility = View.GONE}
+                else -> binding.bottomNavigation.visibility = View.VISIBLE
+            }
+        }
+
+//        onBackPressedDispatcher.addCallback(object : OnBackPressedCallback(true){
+//            override fun handleOnBackPressed() {
+//                binding.fragmentContainerView2.findNavController().navigateUp()
+//            }
+//        })
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp() || super.onSupportNavigateUp()
-    }
+
 }
