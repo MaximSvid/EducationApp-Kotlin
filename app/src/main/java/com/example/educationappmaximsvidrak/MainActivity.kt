0f0dc45bf6296fffe985d2c2dc99ee6a6031ca13
Maxim Svidrak <maximsvid@gmail.com>
2024-08-20 10:38:08 +0200

    20.08.24 nav menu... version 3.15

--- app/src/main/java/com/example/educationappmaximsvidrak/MainActivity.kt
+++ app/src/main/java/com/example/educationappmaximsvidrak/MainActivity.kt
@@ -7,6 +7,7 @@ import android.view.View
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
@@ -24,6 +25,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var drawerLayout: DrawerLayout
 
+    private lateinit var drawerNavController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -31,7 +34,7 @@ class MainActivity : AppCompatActivity() {
 
         drawerLayout = binding.drawerLayout
 
-
+        //нижняя навигация
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         binding.bottomNavigation.setupWithNavController(navHostFragment.navController)
@@ -52,6 +55,16 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
+
+        // боковая навигация
+        val drawerNavHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        binding.drawerNavView.setupWithNavController(drawerNavHostFragment.navController)
+
+        binding.drawerNavView.setNavigationItemSelectedListener { menuItem ->
+            binding.drawerLayout.closeDrawers(GravityCompat.START)
+            drawerNavController.navigate(menuItem.itemId)
+            true
+        }
 
     }
 
